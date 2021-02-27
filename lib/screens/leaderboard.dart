8139f3dd32f318740c9659d29737d85156c03846
itsmihir <inmihir11@gmail.com>
2021-02-27 19:06:55 +0545

    search added

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -1,9 +1,59 @@
+import 'package:animated_background/animated_background.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:snaplify/models/friendShipStatus.dart';
+import 'package:snaplify/models/users.dart';
+import 'package:snaplify/providers/server.dart';
+import 'package:snaplify/widgets/alertDialog.dart';
+import 'package:snaplify/widgets/userGrid.dart';
 
-class LeaderBoard extends StatelessWidget {
-  static const routeName = 'leaderborad';
+class LeaderBoard extends StatefulWidget {
+  static const routeName = '/leaderborad';
+
+  @override
+  State<StatefulWidget> createState() => LeaderboardState();
+}
+
+class LeaderboardState extends State<LeaderBoard>
+    with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AnimatedBackground(
+        behaviour: RandomParticleBehaviour(
+            options: ParticleOptions(
+                baseColor: Colors.purple,
+                spawnMinSpeed: 20,
+                spawnMaxSpeed: 70,
+                spawnMaxRadius: 30)),
+        vsync: this,
+        child: FutureBuilder(
+          future: Provider.of<Server>(context, listen: false)
+              .getFriendsWithPoints(),
+          builder: (_, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError)
+                return CustomAlertDialog(
+                    title: "Something went wrong",
+                    message: "Please check your network",
+                    disableButton: true);
+              List<Users> _friends = snapshot.data;
+              if (_friends.isEmpty) return Center(child: Text("No Friends"));
+
+              _friends.sort((a, b) {
+                if (a.points < b.points)
+                  return 1;
+                else
+                  return 0;
+              });
+
+              return ListView.builder(
+                  itemCount: _friends.length,
+                  itemBuilder: (ctx, i) {
+                    return UserGrid(_friends[i], FriendShipStatus.MyFriend);
+                  });
+            }
+            return Center(child: CircularProgressIndicator());
+          },
+        ));
   }
 }
