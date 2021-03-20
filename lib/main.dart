e487ef26de7a2a2ce88e52e3a0e8382b9beb4a57
itsmihir <inmihir11@gmail.com>
2021-03-20 12:34:44 +0545

    added full image screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,20 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:snaplify/providers/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:snaplify/providers/server.dart';
+import 'package:snaplify/providers/challenge.dart';
+import 'package:snaplify/providers/friendship.dart';
+import 'package:snaplify/providers/notification.dart';
+import 'package:snaplify/providers/user.dart';
 import 'package:snaplify/screens/challengeDetails.dart';
 import 'package:snaplify/screens/challengeGame.dart';
 import 'package:snaplify/screens/chatScreen.dart';
 import 'package:snaplify/screens/home.dart';
 import 'package:snaplify/screens/createChallenge.dart';
 import 'package:snaplify/screens/login.dart';
-import 'package:snaplify/screens/notify.dart';
-import 'package:snaplify/screens/profile.dart';
 import 'package:snaplify/screens/shareChallenge.dart';
 import 'package:snaplify/screens/profileScreen.dart';
+import 'package:snaplify/screens/webView.dart';
+import 'package:snaplify/widgets/fullphoto.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,10 +31,25 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider.value(
           value: Auth(),
         ),
-        ChangeNotifierProxyProvider<Auth, Server>(
-          create: (ctx) => Server(null),
-          update: (ctx, auth, previousOrders) => Server(auth.userId),
-        )
+        ChangeNotifierProxyProvider<Auth, ChallengesProvider>(
+          create: (ctx) => ChallengesProvider(null),
+          update: (ctx, auth, previousOrders) =>
+              ChallengesProvider(auth.userId),
+        ),
+        ChangeNotifierProxyProvider<Auth, FriendDataProvider>(
+          create: (ctx) => FriendDataProvider(null),
+          update: (ctx, auth, previousOrders) =>
+              FriendDataProvider(auth.userDetails),
+        ),
+        ChangeNotifierProxyProvider<Auth, NotificationProvider>(
+          create: (ctx) => NotificationProvider(null),
+          update: (ctx, auth, previousOrders) =>
+              NotificationProvider(auth.userId),
+        ),
+        ChangeNotifierProxyProvider<Auth, UserDataProvider>(
+          create: (ctx) => UserDataProvider(null),
+          update: (ctx, auth, previousOrders) => UserDataProvider(auth.userId),
+        ),
       ],
       child: Consumer<Auth>(
         builder: (ctx, auth, _) => MaterialApp(
@@ -57,10 +75,11 @@ class MyApp extends StatelessWidget {
             ChallengeDoneDetails.routeName: (ctx) => ChallengeDoneDetails(),
             Chat.routeName: (ctx) =>
                 Chat(peer: ModalRoute.of(ctx).settings.arguments),
-            Profile.routeName: (ctx) =>
-                Profile(userId: ModalRoute.of(ctx).settings.arguments),
             ProfileScreen.routeName: (ctx) =>
                 ProfileScreen(userId: ModalRoute.of(ctx).settings.arguments),
+            FullPhoto.routeName: (ctx) =>
+                FullPhoto(url: ModalRoute.of(ctx).settings.arguments),
+            WebView.routeName: (ctx) => WebView()
           },
         ),
       ),
